fe94fa355f5c8bbd2810cb1b84ed259bb0de9c0e
Ross Williams <rossjrw@gmail.com>
2019-03-04 01:45:08 +0000

    Remove stray plugin

--- logmessage.py
+++
@@ -1,9 +0,0 @@
-# Parses messages.
-# Not a plugin.
-import re
-
-class ParsedMessage():
-    def __init__(self, message):
-        # Make sure the message is a string
-        message = str(message)
-        # Was TARS pinged?
