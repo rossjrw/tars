2d0e640ab4a7134125d8497cd4d8169c61aed70c
Ross Williams <rossjrw@gmail.com>
2019-04-05 13:26:13 +0100

    Update, presumably

--- plugins/names.py
+++ plugins/names.py
@@ -10,6 +10,29 @@ class Names:
     def __init__(self, irc_c, config):
         print("Names Plugin Loaded!")
 
-    @observe('IRC_MSG')
+    @observe('IRC_MSG_353') # 353 is a NAMES response
     def record_names(self, irc_c, msg):
-        print("[Names] {}".format(msg.kind))
+        # the names are in msg.args but as individual letters
+        saving  = False
+        names = []
+        name = ""
+        for letter in msg.args:
+            # we don't want to keep any letters until we get to a colon
+            if saving:
+                if letter is not ' ':
+                    name += letter
+                else:
+                    names.append(name)
+                    name = ""
+            elif letter == ':':
+                saving = True
+        # chatstaff names start with a punctuation
+        # for consistency, let's add a char to identify names with no role
+        for key,name in enumerate(names):
+            print(name[0])
+            if name[0] in "~&@%+":
+                # they have a role, do nothing
+                pass
+            else:
+                names[key] = "-" + name
+        print(names)
