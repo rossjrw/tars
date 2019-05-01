ff157c98b3ae5692489a6bbde088c7c0f0c2b5e3
Ross Williams <rossjrw@gmail.com>
2019-05-01 08:23:26 +0100

    Begin db_driver replacement

--- plugins/db.py
+++ plugins/db.py
@@ -1,6 +1,29 @@
 """database.py
 
-Plugin wrapper for helpers/database.py (?)
-"""
+SQLite3 database drive for TARS.
 
-from pyaib import 
+Plugin responsible for accessing and modifying the database.
+ALL database queries MUST pass through this file.
+Provides functions for manipulating the database.
+"""
+# reminder: conn.commit() after making changes (i.e. not queries)
+# reminder: 'single quotes' for string literals eg for tables that don't exist
+
+from pyaib.db import db_driver
+import sqlite3
+
+# mark this file as the driver instead of pyaib.dbd.sqlite
+# also set by db.backend in the config
+@db_driver
+class SqliteDriver:
+    """SQLite3 database driver"""
+    def __init__(self, config):
+        path = config.path
+        if not path:
+            raise RuntimeError("Missing 'path' config for database driver")
+        try:
+            self.conn = sqlite3.connect(path)
+        except sqlite3.OperationalError as e:
+            # can't open db!
+            raise
+        print("Database Driver loaded!")
