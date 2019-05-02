930b60d41c3a2b420a8163d0d10ba01d6838ae2a
Ross Williams <rossjrw@gmail.com>
2019-05-02 14:55:10 +0100

    Add column defualts

--- helpers/database.py
+++ helpers/database.py
@@ -30,68 +30,88 @@ class SqliteDriver:
         self.create_database()
 
     def create_database(self):
+        """Create tables if they don't already exist"""
         c = self.conn.cursor()
         c.execute("""
-          CREATE TABLE IF NOT EXISTS channels (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            channel_name TEXT NOT NULL,
-            date_checked TEXT NOT NULL,
-            autojoin BOOLEAN NOT NULL CHECK (autojoin IN (0,1)),
-            helen_active BOOLEAN NOT NULL CHECK (helen_active IN (0,1))
-         );""")
+            CREATE TABLE IF NOT EXISTS channels (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                channel_name TEXT NOT NULL,
+                date_checked TEXT NOT NULL
+                    DEFAULT CURRENT_TIMESTAMP,
+                autojoin BOOLEAN NOT NULL
+                    CHECK (autojoin IN (0,1))
+                    DEFAULT 1,
+                helen_active BOOLEAN NOT NULL
+                    CHECK (helen_active IN (0,1))
+                    DEFAULT 0
+            );""")
         # users stores wiki, irc and discord users
         c.execute("""
-          CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            controller BOOLEAN NOT NULL CHECK (controller IN (0,1)),
-            seen TEXT NOT NULL
-         );""")
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                controller BOOLEAN NOT NULL
+                    CHECK (controller IN (0,1)),
+                    DEFAULT 0
+            );""")
         c.execute("""
-          CREATE TABLE IF NOT EXISTS channels_users (
-            channel_id INTEGER NOT NULL,
-            user_id INTEGER NOT NULL,
-            user_mode CHARACTER(1),
-            FOREIGN KEY(channel_id) REFERENCES channels(id),
-            FOREIGN KEY(user_id) REFERENCES users(id)
-         );""")
+            CREATE TABLE IF NOT EXISTS channels_users (
+                channel_id INTEGER NOT NULL,
+                user_id INTEGER NOT NULL,
+                user_mode CHARACTER(1),
+                FOREIGN KEY(channel_id) REFERENCES channels(id),
+                FOREIGN KEY(user_id) REFERENCES users(id)
+            );""")
         c.execute("""
-          CREATE TABLE IF NOT EXISTS user_aliases (
-            user_id INTEGER NOT NULL,
-            alias TEXT NOT NULL,
-            type TEXT NOT NULL CHECK (type IN ('irc','wiki','discord')),
-            FOREIGN KEY(user_id) REFERENCES users(id)
-         );""")
+            CREATE TABLE IF NOT EXISTS user_aliases (
+                user_id INTEGER NOT NULL,
+                alias TEXT NOT NULL,
+                type TEXT NOT NULL
+                    CHECK (type IN ('irc','wiki','discord')),
+                FOREIGN KEY(user_id) REFERENCES users(id)
+            );""")
         c.execute("""
-          CREATE TABLE IF NOT EXISTS articles (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            url TEXT NOT NULL UNIQUE,
-            title TEXT NOT NULL,
-            scp_num TEXT,
-            parent TEXT,
-            is_promoted BOOLEAN NOT NULL CHECK (is_promoted IN (0,1)),
-            date_posted TEXT NOT NULL,
-            date_checked TEXT NOT NULL,
-            ups INTEGER NOT NULL,
-            downs INTEGER NOT NULL
-         );""")
+            CREATE TABLE IF NOT EXISTS articles (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                url TEXT NOT NULL UNIQUE,
+                title TEXT NOT NULL,
+                scp_num TEXT,
+                parent TEXT,
+                ups INTEGER NOT NULL,
+                downs INTEGER NOT NULL,
+                date_posted TEXT NOT NULL,
+                is_promoted BOOLEAN NOT NULL
+                    CHECK (is_promoted IN (0,1))
+                    DEFAULT 0,
+                date_checked TEXT NOT NULL
+                    DEFAULT CURRENT_TIMESTAMP
+            );""")
         c.execute("""
-          CREATE TABLE IF NOT EXISTS articles_tags (
-            article_id INTEGER NOT NULL,
-            tag TEXT NOT NULL,
-            FOREIGN KEY(article_id) REFERENCES articles(id)
-         );""")
+            CREATE TABLE IF NOT EXISTS articles_tags (
+                article_id INTEGER NOT NULL,
+                tag TEXT NOT NULL,
+                FOREIGN KEY(article_id) REFERENCES articles(id)
+            );""")
         c.execute("""
-          CREATE TABLE IF NOT EXISTS articles_authors (
-            article_id INTEGER NOT NULL,
-            author TEXT NOT NULL,
-            FOREIGN KEY(article_id) REFERENCES articles(id)
-         );""")
+            CREATE TABLE IF NOT EXISTS articles_authors (
+                article_id INTEGER NOT NULL,
+                author TEXT NOT NULL,
+                FOREIGN KEY(article_id) REFERENCES articles(id)
+            );""")
         c.execute("""
-          CREATE TABLE IF NOT EXISTS showmore_list (
-            channel_id INTEGER NOT NULL,
-            id INTEGER NOT NULL,
-            article_id INTEGER NOT NULL,
-            FOREIGN KEY(channel_id) REFERENCES channels(id)
-         );""")
+            CREATE TABLE IF NOT EXISTS showmore_list (
+                channel_id INTEGER NOT NULL,
+                id INTEGER NOT NULL,
+                article_id INTEGER NOT NULL,
+                FOREIGN KEY(channel_id) REFERENCES channels(id)
+            );""")
         # Will also need a messages table for each channel
         self.conn.commit()
+
+    def join_channel(self, channel):
+        """Populate a new channel in the database"""
+        # create a new entry in channels
+        c = self.conn.cursor()
+        c.execute("""
+            INSERT INTO channels
+                (channel_name, date_checked, autojoin)
+            VALUES (
