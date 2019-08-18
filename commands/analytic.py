c76ec1e2ffed288c23f6e00292904408e42a8696
Ross Williams <rossjrw@gmail.com>
2019-08-18 05:02:00 +0100

    Actually implement the fix

--- commands/analytic.py
+++ commands/analytic.py
@@ -222,9 +222,8 @@ class gib:
             sentence = match.group(2).strip()
         # second: modify any words that match the names of channel members
         members = DB.get_channel_members(msg.channel)
-        members = re.compile(r"\b" + r"\b|\b".join(members) + "\b",
+        members = re.compile(r"\b" + r"\b|\b".join(members) + r"\b",
                              flags=re.IGNORECASE)
-        print(members.search(sentence))
         if msg.channel is not None:
             sentence = members.sub(cls.obfuscate, sentence)
         msg.reply(sentence)
