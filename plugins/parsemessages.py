c50a522ca2264436359d54406469b9576f28e5b7
Ross Williams <rossjrw@gmail.com>
2020-01-30 23:02:36 +0000

    Move repeat logic into a function

--- plugins/parsemessages.py
+++ plugins/parsemessages.py
@@ -9,24 +9,30 @@ from pyaib.plugins import observe, plugin_class
 from helpers import parse
 from helpers.error import CommandError, CommandNotExistError, MyFaultError
 
-def try_command(command_name, irc_c, msg, cmd):
+def try_command(irc_c, msg, cmd, command_name=None):
     """Execute the command of the given name."""
+    if command_name is None:
+        command_name = cmd.command
     try:
         # Call the command from the right file in commands/
         # getattr instead of commands[cmd] bc module subscriptability
         command_class = getattr(commands.COMMANDS, command_name)
         command_class.command(irc_c, msg, cmd)
+        return 0
     except CommandNotExistError:
         if cmd.pinged:
             # there are .converse strings for pinged
-            try_command('converse', irc_c, msg, cmd)
+            return try_command('converse', irc_c, msg, cmd)
         elif msg.channel is None:
             # should be only in pm
-            msg.reply("That's not a command.")
+            msg.reply("I don't know what '{}' means.".format(command_name))
+            return 1
     except CommandError as exc:
         msg.reply("\x02Invalid command:\x0F {}".format(str(exc)))
+        return 1
     except MyFaultError as exc:
         msg.reply("\x02Sorry!\x0F {}".format(str(exc)))
+        return 1
     except Exception as exc:
         if msg.channel != '#tars':
             msg.reply(("An unexpected error has occurred. "
@@ -38,6 +44,34 @@ def try_command(command_name, irc_c, msg, cmd):
                                 .format(msg.sender, msg.message, exc)))
         raise
 
+def execute_commands(irc_c, msg, cmds, command_name=None):
+    """Executes a series of commands."""
+    for cmd in cmds:
+        # reload command takes highest priority
+        if cmd.command == "reload":
+            # special case for .reload - needs high priority
+            msg.reply("Reloading commands...")
+            try:
+                reload(commands)
+            except:
+                msg.reply("Reload failed.")
+                raise
+            else:
+                msg.reply("Reload successful.")
+            continue
+        # indiciate quotemark parse error
+        if cmd.quote_error:
+            msg.reply(("I wasn't able to correctly parse your quotemarks, "
+                       "so I have interpreted them literally."))
+        # assume converse if no command specified
+        if not cmd.command:
+            command_name = 'converse'
+        # do not catch error if this fails
+        command_failed = try_command(irc_c, msg, cmd, command_name)
+        # only progress to next command if previous passed
+        if command_failed:
+            break
+
 @plugin_class('parsemessages')
 class ParseMessages():
     def __init__(self, irc_c, config):
@@ -46,38 +80,9 @@ class ParseMessages():
 # TODO: if plugins are just object instances, then we should be able to
 # wipe em and remake em to .reload
     @observe("IRC_MSG_PRIVMSG")
-    def handleMessage(self, irc_c, msg):
-        submessages = msg
-        cmd = parse.ParsedCommand(irc_c, msg)
-        # cmd is the parsed msg (used to be msg.parsed)
-        if cmd.command:
-            # this is a command!
-            if cmd.command == "reload":
-                # special case for .reload - needs high priority
-                msg.reply("Reloading commands...")
-                try:
-                    reload(commands)
-                except:
-                    msg.reply("Reload failed.")
-                    raise
-                else:
-                    msg.reply("Reload successful.")
-                return
-            # notify of a shlex error, if present
-            if cmd.quote_error:
-                msg.reply(("I wasn't able to correctly parse your quotemarks, "
-                           "so I have interpreted them literally."))
-            try_command(cmd.command, irc_c, msg, cmd)
-        else:
-            # not a command, and not pinged
-            # Send the message over to .converse
-            try_command('converse', irc_c, msg, cmd)
-        if msg.channel is None:
-            # we're working in PMs
-            pass
-        else:
-            # we're in a channel
-            pass
+    def handle_message(self, irc_c, msg):
+        cmds = parse.parse_commands(irc_c, msg)
+        execute_commands(irc_c, msg, cmds)
 
     @observe('IRC_MSG_INVITE')
     def invited(self, irc_c, msg):
