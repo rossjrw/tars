9e3c9ae0551d580a44da0facbfa825b7170199f1
Ross Williams <rossjrw@gmail.com>
2020-03-28 09:57:17 +0000

    Add base command class

---
+++ commands/_command.py
@@ -0,0 +1,13 @@
+"""
+_command.py
+
+Provides the base _Command class that all commands inherit from.
+"""
+
+class _Command:
+    def __new__(cls):
+        # TODO expandargs
+        pass
+
+    def __init__(self):
+        pass
