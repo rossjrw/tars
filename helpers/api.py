8627d9df89ae7c0f3a2c729e104c16b130222cdb
Ross Williams <rossjrw@gmail.com>
2020-06-20 21:12:12 +0100

    Update keys file from txt to toml

--- helpers/api.py
+++ helpers/api.py
@@ -2,22 +2,25 @@
 
 Provides methods for accessing API.
 
-API keys should be kept in keys.secret.txt in the base directory. Lines must
-be in pairs. The first line must contain the key name and nothing else. The
-second line must contain the key value and nothing else. (Key names should only
-be on odd lines, key values should only be on even lines.)
+API keys should be kept in keys.secret.toml in the base directory. This is a
+TOML file containing a single table named 'keys' containing each key with its
+value as a string.
 
-Valid key names are:
+The keys are:
+    irc_password: The bots' IRC NickServ password.
+    google_cse_api: The API key for the mismatch search via Google CSE.
+    google_cse_id: The ID of the Google CSE.
+    scuttle_api: A Personal Access Token for SCUTTLE.
 """
+import os.path
+from xmlrpc.client import ServerProxy
+import json
+
+import urllib3
+
 possible_keys = ["irc_password", "wikidot_api", "google_cse_api",
                  "google_cse_id", "scuttle_api", "scuttle_oauth_id",
                  "scuttle_oauth_secret"]
-
-import os.path
-from xmlrpc.client import ServerProxy
-import urllib3
-from pprint import pprint
-import json
 
 http = urllib3.PoolManager()
 
@@ -29,7 +32,7 @@ with open(os.path.dirname(__file__) + "/../keys.secret.txt") as file:
     google_api_key = keylist['google_cse_api']
     cse_key = keylist['google_cse_id']
 
-class WikidotAPI:
+class ScuttleAPI:
     """Wrapper for Wikidot API functions."""
     def __init__(self, wikiname):
         self.w = wikiname
@@ -98,28 +101,6 @@ class WikidotAPI:
             })
         return(json.loads(r.data.decode('utf-8'))['message'])
 
-SCPWiki = WikidotAPI("scp-wiki")
-Sandbox3 = WikidotAPI("scp-sandbox-3")
-Topia = WikidotAPI("topia")
-
-def get_ups(article):
-    r = http.request(
-        'GET',
-        "http://www.scp-wiki.net/ajax-module-connector.php",
-        headers={
-            'Cookie': "wikidot_udsession=1; {}; {};".format(cookie, token7),
-            'Origin': "http://www.scp-wiki.net",
-            'Referer': "http://www.scp-wiki.net/{}".format(article.url),
-            'Accept-Encoding': "gzip, deflate",
-            'Accept-Language': "en-US,en;q=0.8",
-            'User-Agent': "TARS",
-            'Content-Type': "application/x-www-form-urlencoded; charset=UTF-8",
-            'Accept': "*/*",
-            'X-Requested-With': "XMLHttpRequest",
-            'Connection': "keep-alive"
-        }
-    )
-
-def get_id(fullname):
-    """Gets the Wikidot ID of a page."""
-    pass
+SCPWiki = ScuttleAPI("scp-wiki")
+Sandbox3 = ScuttleAPI("scp-sandbox-3")
+Topia = ScuttleAPI("topia")
