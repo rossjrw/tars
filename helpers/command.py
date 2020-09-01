fc670f256cb6d4f1254d581200bf921e419e6893
Ross Williams <rossjrw@gmail.com>
2020-09-01 11:53:06 +0100

    Add defaults to all arguments, including sentinel

--- helpers/command.py
+++ helpers/command.py
@@ -17,7 +17,8 @@ from helpers.error import (
 )
 
 
-Empty = object()
+# Sentinel value to indicate that an argument was not provided
+NoArgument = object()
 
 
 def regex_type(validation_regex, validation_reason):
@@ -72,8 +73,7 @@ class HelpFormatter(argparse.HelpFormatter):
     def _get_default_metavar_for_optional(self, action):
         """Change the default metavar for optional arguments to the long name
         of that argument instead of its uppercase"""
-        # return action.dest
-        return action.type.__name__
+        return action.dest
 
 
 def help_formatter(prog):
@@ -189,17 +189,17 @@ class Command:
                 raise ValueError("arg must have help string")
             # 5. Handle the nargs
             if 'nargs' in arg:
-                if arg['nargs'] in ['*', '+']:
-                    # default to empty list instead of None
-                    arg['default'] = []
-                # For optional arguments, the default value is taken from const
-                # (otherwise it's None, which falsely indicates the argument is
-                # not present
-                if arg['nargs'] == '?':
-                    if 'const' not in arg:
-                        raise ValueError("must provide const with nargs '?'")
-                    if arg['const'] is None:
-                        raise TypeError("optional const may not be None")
+                # Assign sensible defaults
+                if 'default' not in arg:
+                    if arg['nargs'] in ['*', '+']:
+                        arg['default'] = []
+                    else:
+                        # The default would usually be None
+                        # Use __contains__ to check if argument is present
+                        arg['default'] = NoArgument
+                else:
+                    if arg['nargs'] in ['*', '+']:
+                        assert isinstance(arg['default'], list)
             parser.add_argument(*flags, **arg)
         # Add a hidden argument that takes the remainder of the command
         # these will be later added to the root argument
@@ -210,19 +210,28 @@ class Command:
 
     def __contains__(self, arg):
         """Checks for argument presence"""
-        if arg in self.args and isinstance(getattr(self.args, arg), bool):
-            print(arg, self)
+        # All arguments should have a default value of some sort
+        if arg not in self.args:
             raise AttributeError(
-                "Using 'in' to check for presence of boolean argument"
+                "arg {} doesn't exist on {}".format(arg, self.args)
             )
-        return (
-            arg in self.args
-            and getattr(self.args, arg) is not None
-            and getattr(self.args, arg) != []
-        )
+        # If the value is the sentinel then the argument was not provided AND
+        # there is no default
+        if getattr(self.args, arg) is NoArgument:
+            return False
+
+        return True
 
     def __getitem__(self, arg):
         """Retrieves argument value via getitem operator"""
+        # Cannot access item is the argument was either not provided or if it
+        # has no default value
+        if getattr(self.args, arg) is NoArgument:
+            raise AttributeError(
+                "tried to access an argument ({}) that either wasn't given "
+                "or has no default value".format(arg)
+            )
+        # Return either the given value or the default value
         return getattr(self.args, arg)
 
     def __len__(self):
