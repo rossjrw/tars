22c42c8e431fba2d92f7f702d86d7fc8a5ea3741
Ross Williams <rossjrw@gmail.com>
2020-09-22 18:52:01 +0100

    Add scheduler plugin with initial example

--- plugins/scheduler.py
+++ plugins/scheduler.py
@@ -2,3 +2,25 @@
 
 Exports a schedule object which is used to schedule looping tasks
 """
+
+from apscheduler.schedulers.gevent import GeventScheduler
+from pyaib.plugins import plugin_class
+
+from helpers.config import CONFIG
+
+
+def send_a_message_to_home(irc_c):
+    irc_c.PRIVMSG(CONFIG.home, "hello!")
+
+
+@plugin_class("scheduler")
+class Schedule:
+    def __init__(self, irc_c, config):
+        self.config = config
+        self.scheduler = GeventScheduler()
+
+        self._scheduler_greenlet = self.scheduler.start()
+
+        self.scheduler.add_job(
+            send_a_message_to_home, 'cron', args=[irc_c], second="0,30"
+        )
