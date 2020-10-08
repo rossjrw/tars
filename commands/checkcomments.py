5308b1054561a0d07a1ac90f2cfa9c94a288da83
Ross Williams <rossjrw@gmail.com>
2020-10-08 23:12:42 +0100

    Add template strings (without plurals)

--- commands/checkcomments.py
+++ commands/checkcomments.py
@@ -6,6 +6,85 @@ Checks your posts for replies you may have missed.
 from helpers.database import DB
 from helpers.error import CommandError, MyFaultError
 
+FORUM_URL = "http://scp-wiki.wikidot.com/forum"
+
+REPORT_BODY = """
+{intro}
+\\n\\n
+{info}
+\\n\\n
+{forums}
+\\n\\n
+{footer}
+"""
+
+REPORT_INTRO = """
+# .checkcomments/.cc report
+\\n\\n
+`.checkcomments/.cc` is a [{botname}]({repo}) command that
+searches forum threads on SCP Wiki EN for replies you might have missed.
+\\n\\n
+This report will notify you of any replies to posts that you've made, or
+replies to threads that you started. You can also [manually subscribe and
+unsubscribe]({subscription_thread}) from threads. {botname} doesn't know which
+threads are the discussion pages of articles you've posted — hopefully you
+started that thread by making an author post. If not, you will need to manually
+subscribe to it.
+"""
+
+REPORT_INFO = """
+### Report for {username} :mailbox{mailbox_status}:
+\\n\\n
+This is a report to check comments for Wikidot user
+[{username}](https://www.wikidot.com/user:info/{wd_username}) made since
+{time_context} {date}. This report was initiated by IRC user {init_nick}
+(`{init_hostmask}`) at {init_date}.
+\\n\\n
+You are subscribed to {sub_thread_count} threads and {sub_post_count} posts, including
+{man_sub_count} manual subscriptions and {man_unsub_count} manual
+unsubscriptions. You have {comment_count} new comments across {thread_count}
+threads in {forum_count} forums.
+\\n\\n
+{forums}
+"""
+
+REPORT_FORUM = """
+##### {forum}
+\\n\\n
+{comment_count} new comments across {thread_count} threads.
+\\n\\n
+{comments}
+"""
+
+REPORT_THREAD = """
+1. **:mailbox_with_mail: Thread: [{title}]({url}/t-{wd_thread_id})**
+\\n
+{replies}
+"""
+
+REPORT_THREAD_REPLY = """
+  - :envelope: Reply:
+[{title}]({url}/t-{wd_thread_id}/#post-{wd_post_id})
+by {author} at {date} (page {pageno})
+"""
+
+REPORT_THREAD_POST = """
+  - :package: Replies to your post:
+[{title}]({url}/t-{wd_thread_id}/#post-{wd_post_id}) (page {pageno})
+\\n
+{replies}
+"""
+
+REPORT_THREAD_POST_REPLY = """
+    - :envelope: Reply:
+[{title}]({url}/t-{wd_thread_id}/#post-{wd_post_id})
+by {author} at {date} (page {pageno})
+"""
+
+REPORT_FOOTER = """
+# :mailbox_with_no_mail:
+"""
+
 
 class checkcomments:
     """Checks for new comments."""
