e73dd83273b44f16e8db6d3c041ae48c0861112c
Ross Williams <rossjrw@gmail.com>
2021-02-22 15:17:49 +0000

    Process info commands

--- commands/info.py
+++ commands/info.py
@@ -8,6 +8,7 @@ import time
 
 import platform, distro
 
+from helpers.basecommand import Command
 from helpers.config import CONFIG
 from helpers.error import CommandError
 from helpers.greetings import acronym
@@ -16,20 +17,36 @@ from helpers.defer import defer
 start_time = time.time()
 
 
-class help:
-    @classmethod
-    def execute(cls, irc_c, msg, cmd):
-        if defer.check(cmd, 'jarvis', 'Secretary_Helen'):
-            return
+class Help(Command):
+    """Provides documentation for bot usage."""
+
+    command_name = "help"
+    defers_to = ["jarvis", "Secretary_Helen"]
+    arguments = [
+        dict(
+            flags=['command'],
+            type=str,
+            nargs=None,
+            help="""A command to get specific help for.
+
+            Will link to that part of the documentation directly.
+            """,
+        )
+    ]
+
+    def execute(self, irc_c, msg, cmd):
         msg.reply(
             "Command documentation: https://git.io/TARS.help. Start a "
             "command with .. to force me to respond."
         )
 
 
-class status:
-    @classmethod
-    def execute(cls, irc_c, msg, cmd):
+class Status(Command):
+    """Shows how long the bot has been alive for."""
+
+    command_name = "status"
+
+    def execute(self, irc_c, msg, cmd):
         msg.reply(
             "{} · run by {} · Python {} · {} · alive for {} · {}".format(
                 acronym(),
@@ -44,49 +61,72 @@ class status:
         )
 
 
-class github:
-    """Provide links to the github"""
+class Github(Command):
+    """Links to the bot's repository."""
 
-    @classmethod
-    def execute(cls, irc_c, msg, cmd):
-        if len(cmd.args['root']) > 0:
-            if cmd.args['root'][0].startswith('i'):
-                msg.reply("{}/issues".format(CONFIG.repository))
-            elif cmd.args['root'][0].startswith('p'):
-                msg.reply("{}/pulls".format(CONFIG.repository))
-        else:
-            msg.reply(CONFIG.repository)
+    arguments = [
+        dict(
+            flags=['section'],
+            type=str,
+            nargs=None,
+            default="",
+            choices=["i", "p"],
+            help="""The part of the repository to link to.""",
+        )
+    ]
 
-
-class user:
-    """Provide link to a user's Wikidot page"""
-
-    @classmethod
-    def execute(cls, irc_c, msg, cmd):
-        if defer.check(cmd, 'Secretary_Helen'):
-            return
-        if len(cmd.args['root']) > 0:
-            msg.reply(
-                "http://www.wikidot.com/user:info/{}".format(
-                    "-".join(cmd.args['root'])
-                )
+    def execute(self, irc_c, msg, cmd):
+        msg.reply(
+            "{}/{}".format(
+                CONFIG['repository'],
+                {'': "", 'i': "/issues", 'p': "/pulls"}[self['section']],
             )
-        else:
-            raise CommandError("Specify a user's Wikidot username")
+        )
 
 
-class tag:
-    """Provide a link to a tag's page"""
+class User(Command):
+    """Provides a link to a user's Wikidot page."""
 
-    @classmethod
-    def execute(cls, irc_c, msg, cmd):
-        if len(cmd.args['root']) == 1:
-            msg.reply(
-                "http://www.scp-wiki.wikidot.com/system:page-tags/tag/{}".format(
-                    cmd.args['root'][0]
-                )
+    command_name = "user"
+    defers_to = ["Secretary_Helen"]
+    arguments = [
+        dict(
+            flags=['user'],
+            type=str,
+            nargs='+',
+            required=True,
+            help="""The name of the user.""",
+        )
+    ]
+
+    def execute(self, irc_c, msg, cmd):
+        msg.reply(
+            "http://www.wikidot.com/user:info/{}".format(
+                " ".join(self['user']).replace(" ", "-")
             )
-        elif len(cmd.args['root']) == 0:
-            raise CommandError("Specify a tag")
-        else:
-            raise CommandError("Specify just one tag")
+        )
+
+
+class Tag(Command):
+    """Provides a link to a page that lists all articles with this tag.
+
+    See also @command(tags), which is unrelated.
+    """
+
+    command_name = "tag"
+    arguments = [
+        dict(
+            flags=['tag'],
+            type=str,
+            nargs=None,
+            required=True,
+            help="""The chosen tag.""",
+        )
+    ]
+
+    def execute(self, irc_c, msg, cmd):
+        msg.reply(
+            "http://www.scp-wiki.wikidot.com/system:page-tags/tag/{}".format(
+                self['tag']
+            )
+        )
