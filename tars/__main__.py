ac49c7a4fd80e2d8b86c74c911524425bb577e0d
Ross Williams <rossjrw@gmail.com>
2021-02-25 11:36:40 +0000

    Move config getter into __main__

--- tars/__main__.py
+++ tars/__main__.py
@@ -21,7 +21,8 @@ def takeover(bot):
         raise
 
 
-if __name__ == "__main__":
+def get_config_from_command_line():
+    """Gets the specified config file name from the command line invocation"""
     parser = argparse.ArgumentParser(
         description="""Command-line interface for TARS."""
     )
@@ -32,6 +33,11 @@ if __name__ == "__main__":
         help="""The config file for the bot.""",
     )
     args = parser.parse_args()
+    return args.config
+
+
+if __name__ == "__main__":
+    config = get_config_from_command_line()
     # Pyaib wants the config file name and the path separately
-    config_path, _, config_file = args.config.rpartition("/")
+    config_path, _, config_file = config.rpartition("/")
     takeover(IrcBot(config_file, config_path))
--- tars/helpers/config.py
+++ tars/helpers/config.py
@@ -4,24 +4,21 @@ Parses the config file. Totally separate to pyaib's implementation, I promise
 Exports CONFIG, equal to irc_c.config (probably)
 """
 
-import sys
 import yaml
 
 from munch import Munch
 
+from tars.__main__ import get_config_from_command_line
 from tars.helpers.api import toml_url
 
-argv = sys.argv[1:]
-configfile = argv[0] if argv else 'tars.conf'
-
-with open(configfile, 'r') as file:
+with open(get_config_from_command_line(), 'r') as file:
     print("Getting config object...")
     CONFIG = Munch.fromDict(yaml.safe_load(file))
 
 # might save me a few keystrokes
-CONFIG.nick = CONFIG.IRC.nick
-CONFIG.home = CONFIG.channels.home
-CONFIG.owner = CONFIG.IRC.owner
+CONFIG['nick'] = CONFIG['IRC']['nick']
+CONFIG['home'] = CONFIG['channels']['home']
+CONFIG['owner'] = CONFIG['IRC']['owner']
 
 # Grab the additional online config
 print("Getting additional config from external source...")
