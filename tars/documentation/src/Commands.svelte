c46a3befb91df24c9256a95878c33cf46a5ebb6c
Ross Williams <rossjrw@gmail.com>
2021-03-01 00:38:21 +0000

    Style arguments

--- tars/documentation/src/Commands.svelte
+++ tars/documentation/src/Commands.svelte
@@ -6,6 +6,14 @@
       otherInfo => otherInfo.base === info.id
     ).map(otherInfo => otherInfo.id)
   })
+
+  let splitFlag = flag => {
+    const [positional, dash, optional] = flag.split(/^(-{1,2})/)
+    if (positional === "") {
+      return [dash, optional]
+    }
+    return ["", positional]
+  }
 </script>
 
 <h2>command reference</h2>
@@ -14,35 +22,39 @@
   {#each docs.infos as info}
     <div class="command" id={info.id.toLowerCase()}>
       <h3 class="mt-12">
-        {#each info.aliases as alias}<span>{alias}</span>{/each}
+        {#each info.aliases as alias}
+          <span>..</span><span>{alias}</span><span>, </span>
+        {/each}
       </h3>
-      <div class="command-info">
-        {#if info.base === "Command"}
+      {#if info.base === "Command"}
+        <div>
           <pre>..{info.usage}</pre>
-        {/if}
-        {@html info.help}
-        {#if info.base !== "Command"}
-          <p>
-            This command extends {info.base} and supports all of its arguments.
-          </p>
-        {/if}
-        {#if info.subcommands.length > 0}
-          <p>
-            This command is extended by
-            {new Intl.ListFormat("en").format(info.subcommands)}, which
-            {info.subcommands.length === 1 ? "inherits" : "inherit"} all of its
-            arguments.
-          </p>
-        {/if}
-        {#if info.arguments.length > 0}
-          {#each info.arguments as arg}
-            <h4 id={arg.id}>
-              {#each arg.flags as flag}<span>{flag}</span>{/each}
-            </h4>
-            {@html arg.help}
-          {/each}
-        {/if}
-      </div>
+        </div>
+      {/if}
+      {@html info.help}
+      {#if info.base !== "Command"}
+        <p>
+          This command extends {info.base} and supports all of its arguments.
+        </p>
+      {/if}
+      {#if info.subcommands.length > 0}
+        <p>
+          This command is extended by
+          {new Intl.ListFormat("en").format(info.subcommands)}, which
+          {info.subcommands.length === 1 ? "inherits" : "inherit"} all of its
+          arguments.
+        </p>
+      {/if}
+      {#if info.arguments.length > 0}
+        {#each info.arguments as arg}
+          <h4 id={arg.id}>
+            {#each arg.flags.map(splitFlag) as flag}
+              <span>{flag[0]}</span><span>{flag[1]}</span><span>, </span>
+            {/each}
+          </h4>
+          {@html arg.help}
+        {/each}
+      {/if}
     </div>
   {/each}
 </section>
