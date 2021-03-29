869675f920759358c2c8c0a81433461478035bd3
Ross Williams <rossjrw@gmail.com>
2021-03-29 20:54:05 +0100

    Add methods for getting intro lines

--- tars/helpers/basecommand.py
+++ tars/helpers/basecommand.py
@@ -322,6 +322,35 @@ class Command(ABC):
         """Get the number of arguments given to this command"""
         return len(vars(self.args))
 
+    @classmethod
+    def get_intro(cls, *, argument=None, flag=None):
+        """Retrieves the intro line for this command or the given argument."""
+        intro = lambda string: " ".join(string.split("\n\n")[0].split("\n"))
+        if flag is not None:
+            argument = cls.get_argument(flag)
+        if argument is not None:
+            return intro(argument['help'])
+        if argument is not None or flag is not None:
+            # Shouldn't happen, but just to be sure
+            return None
+        if cls.__doc__ is None:
+            return None
+        return intro(cls.__doc__)
+
+    @classmethod
+    def get_argument(cls, flag):
+        """Gets an argument that matches the given flag. Hyphens optional."""
+        flag = flag.strip("-")
+        try:
+            return next(
+                filter(
+                    lambda arg: any(f.endswith(flag) for f in arg['flags']),
+                    cls.arguments,
+                )
+            )
+        except StopIteration:
+            return None
+
     @abstractmethod
     def execute(self, irc_c, msg, cmd):
         """Method to be called when this command is run that implements its
