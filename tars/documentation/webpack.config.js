433a763225da0a08d2c2f5d4948419b89c2ff3ae
Ross Williams <rossjrw@gmail.com>
2021-04-04 00:50:32 +0100

    Emit <style> CSS for Webpack pickup

--- tars/documentation/webpack.config.js
+++ tars/documentation/webpack.config.js
@@ -34,6 +34,7 @@ module.exports = {
         use: {
           loader: "svelte-loader",
           options: {
+            emitCss: true,
             compilerOptions: { dev },
           }
         }
